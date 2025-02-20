0758984dbaf6b7f144ebd80a73ab34254eaf264f
Luiz Neto <92181011+Luizneto7@users.noreply.github.com>
2025-02-20 20:52:05 +0000

    Implementação da exibição da carta cadastrada.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,6 +19,7 @@ int main() {
     printf("Bem vindo ao Super Trunfo Países!\n");
     
     //Solicitando ao usuário os dados da carta a ser criada via prompt. E lendo com o scanf() cada valor inserido e atribuíndo as variáveis criadas anteriormente.
+    // Cadastro das Cartas:
     printf("Informe o código da carta informando a sigla do estado e o número: \n");
     scanf("%s", &cardCode);
 
@@ -30,15 +31,8 @@ int main() {
 
     printf("Informe a área da cidade: \n");
     scanf("%lf", &area);
-
-
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
     // Exibição dos Dados das Cartas:
+    printf("Carta %s cadastrada com sucesso!\n\nCódigo da carta: %s\nCidade: %s\nPopulação: %i\nÁrea: %lf\n");
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
